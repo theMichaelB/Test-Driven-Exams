3e62abfe321ebbc4359c6111bed51c9560b29a3f
Michael <michael@azured.io>
2022-05-05 14:59:48 +0100

    fixed typo

--- terraform/azure/main.tf
+++ terraform/azure/main.tf
@@ -248,7 +248,7 @@ resource "azurerm_lb_rule" "http-rule" {
   backend_port                   = 80
   frontend_ip_configuration_name = "PublicIPAddress"
   backend_address_pool_ids       = azurerm_lb_backend_address_pool.bpepool.id
-  probe_id                       = azurerm_lb_probe.http-rule.id
+  probe_id                       = azurerm_lb_probe.kubernetes.id
 }
 
 
